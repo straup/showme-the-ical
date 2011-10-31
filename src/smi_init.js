dd1c1b5d640a4b321ae1769b325686c3304c2b56
Aaron Straup Cope <asc@dieben.spum.org>
2011-10-31 06:15:59 +0100

    check to see if localStorage is present

--- src/smi_init.js
+++ src/smi_init.js
@@ -1,6 +1,9 @@
 var has_filereader = 1;
+var has_localstorage = 1;
 
 function smi_init(){
+
+	// just use modernizr ?
 
 	// safari doesn't support FileReader at all but Chrome does
 	// except for the part where it won't read local files...wtf
@@ -19,4 +22,22 @@ function smi_init(){
 		// sudo make me a class
 		input.setAttribute("style", "font-style:italic;background-color:transparent;border:none;");
 	}
+
+	// localstorage (caching)
+
+	has_localstorage = (typeof(localStorage) !== "undefined") ? 1 : 0;	
+
+	if (! has_localstorage){
+		var input  = document.getElementById("cache_local");
+		input.setAttribute("disabled", "disabled");
+
+		var label = document.getElementById("cache_local_label");
+		label.innerHTML = "caching is disabled (by your browser)";
+
+		// sudo make me a class
+		label.setAttribute("style", "font-style:italic;background-color:transparent;border:none;");
+
+	}
+
+
 }
